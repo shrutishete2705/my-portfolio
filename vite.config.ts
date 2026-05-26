5bf84da24e2397389bf203e7678eae48fc9b5ca2
unknown <shruti ishwarshete>
2026-05-26 17:20:50 +0530

    deploy for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/my-portfolio/",
-});
+  base: mode === "github" ? "/my-portfolio/" : "",
+}));
